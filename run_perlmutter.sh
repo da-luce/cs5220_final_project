0ff0cf19423d1c645e39a195ff7f07312a171f26
abrar-amin <abraramin.school@gmail.com>
2026-04-28 12:44:41 -0400

    new flag for srun

--- run_perlmutter.sh
+++ run_perlmutter.sh
@@ -29,4 +29,4 @@ BATCH=${4:-256}
 
 mkdir -p logs models
 
-srun ./build/rl/train_stratego "$VARIANT" "$SETUP" "$EPISODES" "$BATCH"
+srun --mpi=cray_shasta ./build/rl/train_stratego "$VARIANT" "$SETUP" "$EPISODES" "$BATCH"
