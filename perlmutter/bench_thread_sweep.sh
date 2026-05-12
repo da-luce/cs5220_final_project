d666056cdc2e3b8dfe308bb81615ca40062b2374
Dalton Luce <dcl252@login21.chn.perlmutter.nersc.gov>
2026-05-11 18:24:32 -0700

    feat: update run modules and env vars for multi node support
    Not sure if these changes are ncecessary besides for bench_rank_sweep,
    but it is more concise at the least.

--- perlmutter/bench_thread_sweep.sh
+++ perlmutter/bench_thread_sweep.sh
@@ -20,13 +20,11 @@
 # OMP_NUM_THREADS=32 without oversubscribing the rank's allocation.
 
 module load PrgEnv-gnu
-module load gcc/12.2.0
-module load cray-mpich/8.1.25
 module load cudatoolkit
 
-export MPICH_GPU_SUPPORT_ENABLED=0
 export NCCL_DEBUG=WARN
 export NCCL_NET_GDR_LEVEL=PHB
+export NCCL_IB_HCA=mlx5
 
 # Arguments
 TARGET_EPISODES=${1:-65536}
